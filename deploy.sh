4cb251686d66c458d24f5ed79a62f26304776c70
Will James <wjames@thoughtworks.com>
2016-08-04 21:23:48 +1000

    cleanup on deploy script and require docker

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,10 @@
+IMAGE_NAME="sportnumerics-predictor"
+
 docker --version
 virtualenv env
 source env/bin/activate
 pip install awscli
 eval $(aws ecr get-login --region us-west-2)
-docker build -t sportnumerics-predictor .
-docker tag sportnumerics-predictor:latest 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics-predictor:latest
-docker push 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics-predictor:latest
+docker build -t $IMAGE_NAME .
+docker tag $IMAGE_NAME:latest 265978616089.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME:latest
+docker push 265978616089.dkr.ecr.us-west-2.amazonaws.com/$IMAGE_NAME:latest
