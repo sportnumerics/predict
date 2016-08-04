bc670c738cb1a5b695248e9676474056f7a98f96
Will James <wjames@thoughtworks.com>
2016-08-04 21:21:47 +1000

    update some names and also fix that we dont need to install python code

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ virtualenv env
 source env/bin/activate
 pip install awscli
 eval $(aws ecr get-login --region us-west-2)
-docker build -t sportnumerics .
-docker tag predictor:latest 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics:latest
-docker push 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics:latest
+docker build -t sportnumerics-predictor .
+docker tag sportnumerics-predictor:latest 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics-predictor:latest
+docker push 265978616089.dkr.ecr.us-west-2.amazonaws.com/sportnumerics-predictor:latest
