96067813a56da5b21ba352c356d336f8f715ce50
Will James <wjames@thoughtworks.com>
2016-08-07 21:29:23 +1000

    i need tests.

--- predictor/upload.py
+++ predictor/upload.py
@@ -8,5 +8,5 @@ results_bucket_name = 'sportnumerics-ratings-results'
 def upload_to_s3(directory, object):
     object_string = json.dumps(object)
     s3 = boto3.client('s3')
-    filename = '{}/ratings.json'.format(directory, dt)
+    filename = '{}/ratings.json'.format(directory)
     s3.upload_fileobj(io.BytesIO(object_string), results_bucket_name, filename)
