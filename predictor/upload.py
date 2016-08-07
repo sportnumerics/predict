1fee16de4ec67dc8759ad1f7c0a1896e8bde2cde
Will James <wjames@thoughtworks.com>
2016-08-07 21:18:39 +1000

    stop versioning by file name.

--- predictor/upload.py
+++ predictor/upload.py
@@ -8,6 +8,5 @@ results_bucket_name = 'sportnumerics-ratings-results'
 def upload_to_s3(directory, object):
     object_string = json.dumps(object)
     s3 = boto3.client('s3')
-    dt = datetime.utcnow().strftime('%Y%m%d%H%M%S')
-    filename = '{}/{}.json'.format(directory, dt)
+    filename = '{}/ratings.json'.format(directory, dt)
     s3.upload_fileobj(io.BytesIO(object_string), results_bucket_name, filename)
