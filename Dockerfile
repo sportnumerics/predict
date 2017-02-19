eaed280d00a12a8a31043a098accd2d36d242d19
Will James <wjames@thoughtworks.com>
2017-02-19 20:23:28 +1100

    Reconfigure worker container slightly

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM python:2
-ADD . /code
 WORKDIR /code
+ADD requirements.txt .
 RUN pip install -r requirements.txt
+ADD predictor .
+ADD go.py .
 CMD python go.py
--- go.py
+++ go.py
@@ -1,8 +1,3 @@
-import sys, os
-
-here = os.path.dirname(os.path.realpath(__file__))
-sys.path.append(os.path.join(here, 'vendored'))
-
 from predictor.run import run
 
 if __name__ == "__main__":
