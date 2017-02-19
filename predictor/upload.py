5d17d0e3de9b9ae814c605d163cbf0e41b54da3d
Will James <wjames@thoughtworks.com>
2017-02-19 22:38:47 +1100

    fix directory not existing. dont need to write local copy in container, so well comment these lines

--- predictor/upload.py
+++ predictor/upload.py
@@ -10,6 +10,6 @@ def upload_to_s3(directory, key, object):
     object_string = json.dumps(object)
     s3 = boto3.client('s3')
     filename = '{}/{}'.format(directory, key)
-    with open(os.path.join('output',filename), 'w') as f:
-        f.write(object_string)
+    # with open(os.path.join('output',filename), 'w') as f:
+    #     f.write(object_string)
     s3.upload_fileobj(io.BytesIO(object_string), results_bucket_name, filename)
