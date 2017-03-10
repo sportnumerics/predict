9aad24d2f65ad1dcb2dd024a9361faa59def5c59
Will James <wjames@thoughtworks.com>
2017-03-11 09:56:05 +1100

    Wait for predict cluster spinup before trying to run the task on it.

--- bootstrap.py
+++ bootstrap.py
@@ -1,5 +1,16 @@
 import boto3
 import os
+import time
+
+def wait_for_cluster_spin_up(ecs_client, cluster):
+    while True:
+        print('Polling registered container count...')
+        response = ecs_client.describe_clusters(cluster)
+        count = response['clusters'][0]['registeredContainerInstancesCount']
+        if count > 0:
+            print('Container count is positive, continuing.')
+            break
+        time.sleep(10)
 
 def handler(event, context):
     autoscaling = boto3.client('autoscaling')
@@ -8,8 +19,12 @@ def handler(event, context):
         DesiredCapacity=1)
     print(response)
     print('Set desired capacity of prediction cluster to 1')
-    
+
     ecs = boto3.client('ecs')
+    wait_for_cluster_spin_up(
+        ecs_client=ecs,
+        cluster=os.environ['ECS_CLUSTER'])
+
     response = ecs.run_task(
         cluster=os.environ['ECS_CLUSTER'],
         taskDefinition=os.environ['ECS_TASK'],
