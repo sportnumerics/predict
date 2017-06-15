25ab4840fc6bc8333a802bd3efe2e85a0943ec6e
Will James <wjames@thoughtworks.com>
2017-06-16 00:41:44 +1000

    Need to set region before querying cloudformation

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,11 @@
 
 set -e
 
+REGION="ap-southeast-2"
+
 if [ "$LAMBCI_BRANCH" = "master" ]; then
   CDN_STACK_NAME="sportnumerics-explorer-cdn-prod"
+  aws configure set region $REGION
   ACTIVE_DEPLOYMENT=$(aws cloudformation describe-stacks --stack-name $CDN_STACK_NAME --query 'Stacks[0].Outputs[?OutputKey==`ExplorerStageDeployment`].OutputValue' --output text)
   if [ "$ACTIVE_DEPLOYMENT" = "prodgreen" ]; then
     STAGE="prodblue"
@@ -24,10 +27,10 @@ source ./config/env.sh
 unset AWS_SESSION_TOKEN
 
 docker --version
-eval $(aws ecr get-login --region ap-southeast-2)
+eval $(aws ecr get-login --region $REGION)
 docker build -t $IMAGE_NAME .
 
 SLS_DEBUG=* node_modules/.bin/sls deploy --stage=$STAGE --verbose
 
-docker tag $IMAGE_NAME:latest $ACCOUNT.dkr.ecr.ap-southeast-2.amazonaws.com/$IMAGE_NAME:latest
-docker push $ACCOUNT.dkr.ecr.ap-southeast-2.amazonaws.com/$IMAGE_NAME:latest
+docker tag $IMAGE_NAME:latest $ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
+docker push $ACCOUNT.dkr.ecr.$REGION.amazonaws.com/$IMAGE_NAME:latest
