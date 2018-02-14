d18f786754faec36bd7b4db216d2cfbec7097419
Will James <jameswt@gmail.com>
2018-02-13 23:24:56 -0500

    will need to specify region...

--- deploy.sh
+++ deploy.sh
@@ -27,7 +27,7 @@ SLS_DEBUG=* node_modules/.bin/sls deploy --stage=$STAGE --verbose
 IMAGE_NAME="sportnumerics-predictor-$STAGE"
 
 docker --version
-eval $(aws ecr get-login --region $REGION)
+eval $(aws ecr get-login --region $AWS_DEFAULT_REGION)
 docker build -t $IMAGE_NAME .
 
 docker tag $IMAGE_NAME:latest $AWS_ACCOUNT_ID.dkr.ecr.$AWS_DEFAULT_REGION.amazonaws.com/$IMAGE_NAME:latest
