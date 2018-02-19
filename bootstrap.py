064bf43f9fe1eedfbb7f8c3adc2f5b7e348e5fd9
Will James <jameswt@gmail.com>
2018-02-19 11:32:34 -0600

    Move to fargate for predict task

--- bootstrap.py
+++ bootstrap.py
@@ -1,31 +1,9 @@
 import boto3
 import os
-import time
-
-
-def wait_for_cluster_spin_up(ecs_client, cluster):
-    while True:
-        print('Polling registered container count...')
-        response = ecs_client.describe_clusters(clusters=[cluster])
-        count = response['clusters'][0]['registeredContainerInstancesCount']
-        if count > 0:
-            print('Container count is positive, continuing.')
-            break
-        time.sleep(10)
 
 
 def handler(event, context):
-    autoscaling = boto3.client('autoscaling')
-    response = autoscaling.set_desired_capacity(
-        AutoScalingGroupName=os.environ['AS_GROUP_NAME'],
-        DesiredCapacity=1)
-    print(response)
-    print('Set desired capacity of prediction cluster to 1')
-
     ecs = boto3.client('ecs')
-    wait_for_cluster_spin_up(
-        ecs_client=ecs,
-        cluster=os.environ['ECS_CLUSTER'])
 
     response = ecs.run_task(
         cluster=os.environ['ECS_CLUSTER'],
