5c1b51c2151bc2696f197bbc56e416a34a8072db
Will James <jameswt@gmail.com>
2018-07-11 22:38:49 -0500

    Fix divisions copy command, Bucket is not a member of s3 client

--- predictor/s3_service.py
+++ predictor/s3_service.py
@@ -2,8 +2,7 @@ import os
 import boto3
 import json
 
-s3 = boto3.client('s3')
-
+s3_client = boto3.client('s3')
 
 def query_all_teams(year):
     results_bucket = os.environ['TEAMS_BUCKET_NAME']
@@ -17,7 +16,7 @@ def query_all_teams(year):
         }
 
         print('getting {}'.format(key))
-        response = s3.get_object(**query_args)
+        response = s3_client.get_object(**query_args)
 
         return json.load(response['Body'])
 
@@ -39,7 +38,7 @@ def query_all_teams_keys(year):
         if continuation_token:
             query_args['ContinuationToken'] = continuation_token
 
-        result = s3.list_objects_v2(**query_args)
+        result = s3_client.list_objects_v2(**query_args)
         keys.extend(result['Contents'])
         if 'NextContinuationToken' in result:
             continuation_token = result['NextContinuationToken']
@@ -55,5 +54,6 @@ def copy_divisions(run_name, year):
         'Bucket': os.environ['TEAMS_BUCKET_NAME'],
         'Key': key
     }
-    dest = s3.Bucket(os.environ['RESULTS_BUCKET_NAME'])
+    s3_resource = boto3.resource('s3')
+    dest = s3_resource.Bucket(os.environ['RESULTS_BUCKET_NAME'])
     dest.copy(source, key)
